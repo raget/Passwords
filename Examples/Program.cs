515182f421ebdbfd9fbad02157d304055dcf97dd
Radim Göth <radim.goeth@ysoft.com>
2020-07-29 09:09:51 +0200

    Add examples

---
+++ Examples/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Security.Cryptography;
+using Raget.Security.Passwords;
+
+namespace Raget.Security.Passwords.Examples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            const int entropy = 128;
+            
+            // Example 1: Generate password using default random number generator
+            using var defaultPasswordGenerator = new DefaultPasswordGenerator();
+            var password = defaultPasswordGenerator.GenerateAlphanumericPassword(entropy);
+            
+            Console.WriteLine(password);
+            
+            // Example 2: Generate password from custom character pool
+            var pool = PasswordGenerator.Alphanumerics.Union(new int[] {'*', '.', '/', '?', '-', '.', '_'});
+            password = PasswordGenerator.Default.GeneratePassword(entropy, pool);
+            
+            Console.WriteLine(password);
+            
+            // Example 3: Generate password using custom random number generator
+            using (var randomNumberGenerator = new RNGCryptoServiceProvider())
+            {
+                var passwordGenerator = new PasswordGenerator(randomNumberGenerator);
+                password = passwordGenerator.GenerateAlphanumericPassword(entropy);
+            }
+            
+            Console.WriteLine(password);
+        }
+        
+        
+    }
+}
